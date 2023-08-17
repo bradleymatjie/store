b084493d7f30530c754eef0711e8a87f5017b945
bradleymatjie <bradleymatjie.job@gmail.com>
2023-08-17 11:21:26 +0200

    search functionality works

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,35 @@
 import './Home.scss';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { products } from '../../Products';
 import bagIcon from '../../assets/add.png';
 
 export const Home = ({ handleClick }) => {
     const navigate = useNavigate();
+    const [searchQuery, setSearchQuery] = useState('');
 
     const handleProductClick = (product) => {
         navigate(`/store/${product.id}`);
       };
 
+    const filteredProducts = products.filter(product =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+    ); 
+
     return (
         <section className='home'>
             <label htmlFor="">
                 Search Item
-                <input type="text" placeholder='Apple Watch, Samsung S21, Macbook Pro, ...' />
+                <input
+                    type="text"
+                    id="searchInput"
+                    placeholder='Apple Watch, Samsung S21, Macbook Pro, ...'
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
             </label>
             <div className="products">
-                {products.map((product) => (
+                {filteredProducts.map((product) => (
                     <div className="product" key={product.id}>
                         <div className="imageContainer"  onClick={() => handleProductClick(product)}>
                             <img src={product.img} alt="product image" loading='lazy' />
